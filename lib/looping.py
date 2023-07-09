eb637f50dbea2ab971453946b84f05bd09054e19
Kyuhee Lee <kyuhee.lee.1011@gmail.com>
2023-07-09 19:47:54 -0400

    finish

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+  i =10
+  while i > 0:
+    print (i)
+    i=i-1
+    print ("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list = [intergers *intergers for intergers in int_list ]
+    return int_list 
 
 def fizzbuzz():
-    # code goes here!
-    pass
+       for i in range(1, 101):
+        if not i % 15:
+            print("FizzBuzz")
+        elif not i % 5:
+            print("Buzz")
+        elif not i % 3:
+            print("Fizz")
+        else:
+            print(i)
